8b9c1c8df6ece11968f783c24679fd31aad50f5a
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-05-08 09:52:49 +0100

    Action creators, redux and redux thunk set up

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 // Redux setup
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './reducers.js';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancer(applyMiddleware(thunk)),
   );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
