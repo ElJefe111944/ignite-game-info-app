2aafef9b669bbb04fc3eae14cd32f5a8e64dd213
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-05-15 17:34:08 +0100

    animation file created fro fade out effect

---
+++ src/animation.js
@@ -0,0 +1,17 @@
+export const fadeIn = {
+    hidden: {
+        opcacity: 0
+    },
+    show: {
+        opcacity: 1,
+        transition: {
+            duration: 0.75,
+        },
+        exit: {
+            opcacity: 0,
+            transition: {
+                duration: 0.75,
+            },
+        },
+    },
+};
